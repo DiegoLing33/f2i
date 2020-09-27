8c37ab433e87697de34d7871108b648548f08d35
diego <masnimpu@gmail.com>
2020-09-27 19:48:19 +0300

    Added index file

---
+++ readme.md
@@ -0,0 +1,16 @@
+#F2I 0.1.0 &middot; [![GitHub license](https://img.shields.io/badge/license-Apache_2.0-green.svg)](https://github.com/DiegoLing33/f2i/blob/master/LICENSE) [![npm version](https://img.shields.io/npm/v/f2i.svg?style=flat)](https://www.npmjs.com/package/efi)
+
+File to image utility
+
+---
+## Idea
+Right now (version 0.1.0) can only converts text to image and back again, but it contains the very simple algorithm
+that allow encode files in the future. I want to realize the system like "Path to Image", what about store and share your 
+projects in images? Sounds interesting. Ok go...
+
+## How to use?
+###Installation: 
+`yarn global install f2i` or `npm install -g f2i`.
+
+###Encode and decode
+Run `f2i` in the terminal and use UI application. Cli version right now in progress.
--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,89 @@ import {app, BrowserWindow, dialog, ipcMain} from "electron";
 import path from "path";
 import fs from "fs";
 import ImageIO from "./image/ImageIO";
+import child_process from "child_process";
 
-function createWindow () {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 700,
-        webPreferences: {
-            nodeIntegration: true
-        },
-        title: 'F2I Utility',
-        titleBarStyle: "hidden",
-        resizable: false,
-        fullscreenable: false,
-    });
+const ELECTRON_PATH = __dirname.replace("/dist", "") +
+    "/node_modules/.bin/electron";
 
-    ipcMain.handle('text', (event, ...args) => {
-        ImageIO.textToImage(args[0]).then(value => {
-            value.getBase64Async("image/png").then(value1 => win.webContents.send('imgNew', {base: value1, bitmap: value.bitmap}));
-        }).catch();
-    });
-
-    ipcMain.handle('openImage', event => {
-        dialog.showOpenDialog({
-            message: 'Select file to decode',
-            properties: ["openFile"]
-        }).then(value => {
-            if(!value.canceled){
-                ImageIO.imagePathToText(value.filePaths[0]).then(text => {
-                    win.webContents.send('displayText', {text, path: value.filePaths[0]});
-                }).catch();
-            }
-        }) ;
-    });
-
-    ipcMain.handle('saveImage', (event, imageBase64) => {
-        dialog.showSaveDialog({
-            message: 'Save file',
-            defaultPath: `ci-${new Date().getTime()}.png`
-        }).then(value => {
-            if(!value.canceled){
-                const base64Image = imageBase64.split(';base64,').pop();
-                fs.writeFile(value.filePath!, base64Image, {encoding: 'base64'}, err => {
-                    if(err) dialog.showErrorBox('Saving error', err.message);
-                });
-            }
-        });
-    });
-
-    // and load the index.html of the app.
-    win.loadFile(path.resolve(__dirname, "../src/static/index.html"));
+/**
+ * Запускает электрон
+ * @param flags
+ */
+function launchElectron(flags: string[] = []) {
+    flags.push("gui");
+    child_process.exec(path.resolve(ELECTRON_PATH + " ./ " + flags.join(" ")));
 }
 
-app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-    app.quit()
-});
+const cliArgs = process.argv.slice(2);
+if (cliArgs.length === 0) {
+    launchElectron();
+} else {
+    if (cliArgs.indexOf("gui") > -1) {
+        gui(path.resolve("./"))
+    } else {
+        // cli(cliArgs);
+    }
+}
+function gui(wd?: string) {
+    function createWindow() {
+        // Create the browser window.
+        const win = new BrowserWindow({
+            width: 1000,
+            height: 700,
+            webPreferences: {
+                nodeIntegration: true
+            },
+            title: 'F2I Utility',
+            titleBarStyle: "hidden",
+            resizable: false,
+            fullscreenable: false,
+        });
+
+        ipcMain.handle('text', (event, ...args) => {
+            ImageIO.textToImage(args[0]).then(value => {
+                value.getBase64Async("image/png").then(value1 => win.webContents.send('imgNew', {
+                    base: value1,
+                    bitmap: value.bitmap
+                }));
+            }).catch();
+        });
+
+        ipcMain.handle('openImage', event => {
+            dialog.showOpenDialog({
+                message: 'Select file to decode',
+                properties: ["openFile"]
+            }).then(value => {
+                if (!value.canceled) {
+                    ImageIO.imagePathToText(value.filePaths[0]).then(text => {
+                        win.webContents.send('displayText', {text, path: value.filePaths[0]});
+                    }).catch();
+                }
+            });
+        });
+
+        ipcMain.handle('saveImage', (event, imageBase64) => {
+            dialog.showSaveDialog({
+                message: 'Save file',
+                defaultPath: `ci-${new Date().getTime()}.png`
+            }).then(value => {
+                if (!value.canceled) {
+                    const base64Image = imageBase64.split(';base64,').pop();
+                    fs.writeFile(value.filePath!, base64Image, {encoding: 'base64'}, err => {
+                        if (err) dialog.showErrorBox('Saving error', err.message);
+                    });
+                }
+            });
+        });
+
+
+        // and load the index.html of the app.
+        win.loadFile(path.resolve(__dirname, "../src/static/index.html"));
+    }
+    app.whenReady().then(createWindow);
+
+    app.on('window-all-closed', () => {
+        app.quit()
+    });
+}
