ce51f4275b99a2688c424e8c24cba9dc021582b0
diego <diegoling33@gmail.com>
2020-09-30 12:09:18 +0300

    New implementation of the library

--- src/lib.ts
+++ src/lib.ts
@@ -10,7 +10,49 @@
  * @site http://ling.black
  */
 
-import ImageIO from "./image/ImageIO";
-const F2I = ImageIO;
+import ImageIO, {ImageIOResolveImage} from "./image/ImageIO";
+import Jimp from "jimp";
+
+/**
+ * The F2I library path
+ */
+const F2I = {
+    /**
+     * Converts TEXT --> IMAGE
+     * @param text
+     */
+    async textToImage(text: string): Promise<ImageIOResolveImage>{
+        return ImageIO.createImage(text);
+    },
+    /**
+     * Converts FILE --> IMAGE
+     *
+     * Pass file by path
+     *
+     * @param filePath
+     */
+    async filePathToImage(filePath: string): Promise<ImageIOResolveImage>{
+        return ImageIO.createImageFromFile(filePath);
+    },
+
+    /**
+     * Converts IMAGE --> TEXT
+     * @param image
+     */
+    async imageToText(image: Jimp): Promise<string> {
+        return ImageIO.imageToText(image);
+    },
+
+    /**
+     * Converts IMAGE --> TEXT
+     *
+     * Pass image by file path
+     *
+     * @param path
+     */
+    async imagePathToText(path: string): Promise<string>{
+        return ImageIO.imagePathToText(path);
+    }
+}
 
 export default F2I;
